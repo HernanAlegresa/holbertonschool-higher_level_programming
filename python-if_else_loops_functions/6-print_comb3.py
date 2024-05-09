5c7c3429816eb2d87979b60662a798cf6432d2d5
HernanAlegresa <alegresah@gmail.com>
2024-05-09 16:51:53 +0000

    now yes 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for dig1 in range(10):
     for dig2 in range(dig1 + 1, 10):
         if dig1 == dig2:
-            print("{:d}{:d}, ".format(dig1,dig2), end="")
+            print("{:d}{:d}, ".format(dig1, dig2), end="")
         elif dig1 == 8:
-            print("{:d}{:d}".format(dig1,dig2))            
+            print("{:d}{:d}".format(dig1, dig2))
         else:
-            print("{:d}{:d}, ".format(dig1,dig2), end="")
+            print("{:d}{:d}, ".format(dig1, dig2), end="")
