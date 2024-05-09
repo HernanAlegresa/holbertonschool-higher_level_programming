99067c94919749cb61193c17206b56d81c57464e
HernanAlegresa <alegresah@gmail.com>
2024-05-09 16:49:26 +0000

    no space in 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for dig1 in range(10):
         if dig1 == dig2:
             print("{:d}{:d}, ".format(dig1,dig2), end="")
         elif dig1 == 8:
-            print("{:d}{:d} ".format(dig1,dig2))            
+            print("{:d}{:d}".format(dig1,dig2))            
         else:
             print("{:d}{:d}, ".format(dig1,dig2), end="")
