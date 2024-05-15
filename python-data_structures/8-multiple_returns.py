027294851ff66c9090c007ec505fb3d275ca6883
HernanAlegresa <alegresah@gmail.com>
2024-05-15 22:25:45 +0000

    .

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     for char in range(length):
-        if length <= 0:
+        if length == 0:
             return (0, None)
         else:
             first_char = sentence[0]
