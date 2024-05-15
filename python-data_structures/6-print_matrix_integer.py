23dcadc3ba76fc6ea7a275a2c68de97855662f4d
HernanAlegresa <alegresah@gmail.com>
2024-05-15 14:52:55 +0000

    Updated 6 ready to check

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(matrix):
-        if i 
+    for row in range (len(matrix)):
+        for col in range (len(matrix[row])):
+            if col < len(matrix[row]):
+                print("{:d}".format(matrix[row][col]), end=" ")
+            else:
+                print("{:d}".format(matrix[row][col]), end=" ")
+        print()
