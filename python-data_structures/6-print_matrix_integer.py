34a25280ee5655d0a03cad38951f9065f2018a02
HernanAlegresa <alegresah@gmail.com>
2024-05-15 14:53:17 +0000

    Pyco for task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range (len(matrix)):
-        for col in range (len(matrix[row])):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
             if col < len(matrix[row]):
                 print("{:d}".format(matrix[row][col]), end=" ")
             else:
