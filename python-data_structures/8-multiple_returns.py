976fb67c59a8dc937e79574ed422edf9ead204e8
HernanAlegresa <alegresah@gmail.com>
2024-05-15 22:27:07 +0000

    Now 8 and 9 are correct

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    for char in range(length):
-        if length == 0:
-            return (0, None)
-        else:
-            first_char = sentence[0]
-        return (length, first_char)
+    if length == 0:
+        return (0, None)
+    else:
+        first_char = sentence[0]
+    return (length, first_char)
