0071902ba65aaae650bcf4b19de8c96120f71a71
HernanAlegresa <alegresah@gmail.com>
2024-05-19 00:38:17 +0000

    Updated project Python - Exceptions completed

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    elem_to_print = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            elem_to_print += 1
+        except IndexError:
+            break
+    print()
+    return elem_to_print
