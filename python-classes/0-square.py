73b718b27d63a3ab4a4e84777db183c088beb904
HernanAlegresa <alegresah@gmail.com>
2024-05-20 02:46:50 +0000

    .

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''Defines an empty class Square that defines a square.'''
+'''Defines an empty class Square that defines a square.
+'''
 class Square:
-    '''Represents a square.'''
+    '''Represents a square.
+    '''
     pass
