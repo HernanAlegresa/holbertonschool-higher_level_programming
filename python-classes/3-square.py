c33521a9362d37d202821fca8507e64bf6ddc078
HernanAlegresa <alegresah@gmail.com>
2024-05-20 03:20:08 +0000

    .

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,7 +11,7 @@ class Square:
         '''Initialize the square with size
         '''
         self.__size = size
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError('size must be >= 0')
