55a9e0f43339abac0a309d8776ae40f66f36ec85
HernanAlegresa <alegresah@gmail.com>
2024-05-20 02:55:04 +0000

    .

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 '''Defines an empty class Square that defines a square.
 '''
+
+
 class Square:
     '''Represents a square.
     '''
+    
     def __init__(self, size):
         '''Initialize the square with size
         '''
+        
         self.__size = size
--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 '''Defines an empty class Square that defines a square.
 '''
+
+
 class Square:
     '''Represents a square.
     '''
+    
     def __init__(self, size=0):
         '''Initialize the square with size
         '''
