3f0766683e27c921f040188076e5daa373fd85b2
HernanAlegresa <alegresah@gmail.com>
2024-05-26 22:36:26 +0000

    11.

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -14,8 +14,10 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initialize the square with a size."""
+        self.integer_validator("size", size)
         super().__init__(size, size)
 
     def __str__(self):
         """Return the square description."""
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+        return "[Square] {}/{}".format(self._Rectangle__width,
+                                       self._Rectangle__height)
