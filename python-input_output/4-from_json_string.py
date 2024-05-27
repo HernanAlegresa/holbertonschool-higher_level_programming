98d1fbb878bde581347ac4b2e1c65c7fbef75a14
HernanAlegresa <alegresah@gmail.com>
2024-05-27 23:25:00 +0000

    .

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -7,6 +7,6 @@ import json
 
 def from_json_string(my_str):
     """
-    Function returns an object (Python data structure) represented by a JSON string.
+    Function returns an object represented by a JSON string.
     """
     return json.loads(my_str)
