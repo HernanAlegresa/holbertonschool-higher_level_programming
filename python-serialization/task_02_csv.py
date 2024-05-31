712bbd2d0402dd4c7f1deb09fbe7901e9c65acd7
HernanAlegresa <alegresah@gmail.com>
2024-05-31 20:14:41 +0000

    Updated task 2.

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+2. Converting CSV Data to JSON Format
+"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_file):
+    data = []
+    try:
+        with open(csv_file, 'r') as csvfile:
+            dic_reader = csv.DictReader(csv_file)
+            for row in dic_reader:
+                data.append(row)
+    except FileNotFoundError:
+        return False
+
+    with open('data.jason', 'w') as jasonfile:
+        json.dump(data, jsonfile)
+    return True
