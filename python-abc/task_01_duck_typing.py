ef08d0db83597b483fb41f5a5ec15eec3fc22540
HernanAlegresa <alegresah@gmail.com>
2024-05-31 22:44:34 +0000

    .

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -32,7 +32,7 @@ class Circle(Shape):
         return math.pi * self.radius ** 2
 
     def perimeter(self):
-        return math.pi * self.radius * 2
+        return math.pi * abs(self.radius) * 2
 
 
 class Rectangle(Shape):
