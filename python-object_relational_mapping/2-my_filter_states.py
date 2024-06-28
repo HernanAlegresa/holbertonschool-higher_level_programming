67e456bdd90b621a1a43bd4c01d8e7ffd4651bb1
HernanAlegresa <alegresah@gmail.com>
2024-06-28 13:02:56 -0700

    Updated task 2 completed

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+# Importar modulos MySQL para ejecutar consultas a database desde python y sys para acceder a los argumentos.
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Connects to a MySQL database and lists all states in ascending order by id.
+    """
+    database = MySQLdb.connect(  # Establece conexion con la database y agrego los (argumentos) argv dentro.
+        host= "localhost",
+        port= 3306,
+        user= sys.argv[1],
+        passwd= sys.argv[2],
+        db= sys.argv[3],
+    )
+
+    cursor = database.cursor() # Obj cursor para poder interactuar con la database.
+
+    query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4])
+    cursor.execute(query) # Ejecucion de consulta SQL -----> filter by names starting with 'N'
+
+    rows = cursor.fetchall() # Rows son las filas devueltas por la consulta, fetchall las recupera.
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    database.close()
