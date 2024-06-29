b82c81ee617a9de1eb6dd8443a4b34f83c8668ed
HernanAlegresa <alegresah@gmail.com>
2024-06-29 16:28:50 -0700

    Task 14 not done yet.

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,14 +3,12 @@
 Defines a State class and a Base class to map to the 'states' table in the database.
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
 class State(Base):
-    """
-    State class that maps to the 'states' table in the database.
-    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state")
